8d9d1dfb0bdde885feed889f1e1fcc94b7a46c32
Antony Kalathil George <132806238+ant-x-ny@users.noreply.github.com>
2024-07-12 17:31:12 +0530

    Update error.jsx

--- src/components/error.jsx
+++ src/components/error.jsx
@@ -4,7 +4,7 @@ function Error(){
         <div className='BodyError'>
             <title>Error 404</title>
             <div>
-            <h2 style={{marginTop: '400px',marginLeft: '75px', fontFamily: 'monospace',fontSize:'72px',paddingTop: '5px',paddingLeft: '5px'}}>Site Under Construction :(</h2>
+            <h2 style={{marginTop: '275px',marginLeft: '75px', fontFamily: 'monospace',fontSize:'70px',paddingTop: '5px',paddingLeft: '5px'}}>Site Under Construction :(</h2>
     </div>
     <div style={{width: '100%',justifyContent: 'center',display: 'flex',alignItems: 'center',paddingTop: '100px'}}>
         <a href="/"><button id="cmb">Back To HomePage</button></a>
